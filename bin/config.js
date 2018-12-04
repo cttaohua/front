d62668e3b94df056f4d4e78aa8beebc84f2e16d0
陈坦 <you@example.com>
2018-12-04 17:35:30 +0800

    update

---
+++ bin/config.js
@@ -0,0 +1,29 @@
+//正式
+// const port = '80'; 
+// const mysqlConfig = {
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: '123456',
+// 	database: 'taohua'
+// }
+// const setDefaults = {
+// 	cache: true
+// }
+
+//测试
+const port = '3001'; 
+const mysqlConfig = {
+	host: '47.94.227.198',
+	user: 'root',
+	password: 'Lh456123',
+	database: 'taohua'
+}
+const setDefaults = {
+	cache: false
+}
+
+module.exports = {
+	port: port,
+	mysqlConfig: mysqlConfig,
+	setDefaults: setDefaults
+}
