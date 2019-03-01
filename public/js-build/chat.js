474f61e8b6409d6d04a4d86d1aac835f998b1c19
陈坦 <you@example.com>
2019-03-01 22:33:28 +0800

    2019-03-01

--- public/js-build/chat.js
+++ public/js-build/chat.js
@@ -29,7 +29,7 @@ new Vue({
             if(window.location.host.indexOf('localhost')!=-1) {  //本地
                 var conn = '127.0.0.1:3001';
             }else {   //正式环境
-                var conn = 'https://taohuayuanskill.com';
+                var conn = 'wss://taohuayuanskill.com';
             }  
             //加载socket.io.js
             loadScript('https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js', function () {
